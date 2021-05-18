ec88f3615ff1dcbba9ccf9a9cc0453726472a3b4
Mr Myers <myersniiansah@gmail.com>
2021-05-18 11:54:16 -0400

    stubbed out initial movie list

--- src/MovieList/MovieList.js
+++ src/MovieList/MovieList.js
@@ -1,2 +1,13 @@
 import React from 'React';
+import Movie from './MovieList.js';
 
+const MovieList = (movies) => {
+  //map each movie with the like and dislike function passed down
+  <div className="movie-list">
+    {movies.map((movie) => {
+      <Movie  />
+    })}
+  </div>
+};
+
+export default MovieList;

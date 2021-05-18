079169ceea391aee0c630647ab87590bdfd5b298
Mr Myers <myersniiansah@gmail.com>
2021-05-18 11:53:53 -0400

    stubbed out initial movie outline

--- src/Movie/Movie.js
+++ src/Movie/Movie.js
@@ -1,1 +1,13 @@
 import React from 'React';
+
+const Movie = () => {
+  //retrieve the movie's image
+  //add buttons for like and dislike
+  <div className="movie-entry">
+    <div className="movie-body">
+      <img className="movie-image" src={} alt=""/>
+      <button></button>
+      <button></button>
+    </div>
+  </div>
+}

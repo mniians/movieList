3457e61caea9f7335e93d34040183c80800aa540
Mr Myers <myersniiansah@gmail.com>
2021-05-18 21:48:58 -0400

    inital page rendering with api call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MovieList from './MovieList/MovieList.js'
 import './App.css';
 
-class App extends React.Component {
+const App = () => {
+  const url = 'https://developers.themoviedb.org/3/movies/get-popular-movies'
+  let [movies, addMovies] = useState([]);
+  useEffect(() => {
+    fetch(url,{
+      method: "GET",
+      withCredentials: true,
+      headers: {
+        "X-Auth-Token": "",
+        "Content-Type": "application/json"
+      }
+    })
+      .then((resp) =>  resp.json())
+      .then((data)) => {
+        addMovies(movies = data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 
 
-  render () {
-    return (
-      <div className="App">
-        <div className="nav">
-          <span className="Home" />
-          <span className="Favourites" />
-          <span className="Dislikes" />
-        </div>
-        <div className="body">
-          <MovieList movies={["asd","asdads"]}/>
-        </div>
+
+  return (
+    <div className="App">
+      <div className="nav">
+        <span className="Home" />
+        <span className="Favourites" />
+        <span className="Dislikes" />
       </div>
-    )
-  }
+      <div className="body">
+        <MovieList movies={movies}/>
+      </div>
+    </div>
+  )
+
+
 }
 
 export default App;
